15d56c07a2a50da005530fcaab3f93831af7aac4
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-02-04 18:32:25 +0500

    Update Q2.cpp

--- LABS/01/Q2.cpp
+++ LABS/01/Q2.cpp
@@ -4,46 +4,50 @@ using namespace std;
 int main() 
 {
     int n;
-    cout<<"Enter number of students: ";
-    cin >>n;
-    int marksMaths;
-    int marksEnglish;
-    int marksScience;
-    char grade;
-    for(int i=0;i<n;i++)
+    cout << "Enter number of students: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) 
     {
-        cout<<"Marks of student: "<<i+1<<"\n";
-        cout<<"Marks in Mathematics: ";
-        cin>> marksMaths;
-        cout<<"Marks in English: ";
-        cin>> marksEnglish;
-        cout<<"Marks in Science: ";
-        cin>> marksScience;
-        int totalMarks= marksMaths+marksEnglish+marksScience;
-        int avgMarks = totalMarks/3;
-        if(avgMarks>90)
-        {
+      cout << "Marks of student " << i + 1 << ":\n";
+
+      int marksMaths, marksEnglish, marksScience;
+
+      cout << "Marks in Mathematics: ";
+      cin >> marksMaths;
+
+      cout << "Marks in English: ";
+      cin >> marksEnglish;
+
+      cout << "Marks in Science: ";
+      cin >> marksScience;
+
+      int totalMarks = marksMaths + marksEnglish + marksScience;
+      int avgMarks = totalMarks / 3;
+
+      char grade;
+
+      if (avgMarks > 90) 
+      {
             grade = 'A';
-        }
-        else if(avgMarks>=80 && avgMarks<90)
-        {
+      } else if (avgMarks >= 80) 
+      {
             grade = 'B';
-        }
-        else if(avgMarks>=70 && avgMarks<80)
-        {
-            grade= 'C';
-        }
-        else if(avgMarks>=60 && avgMarks<70)
-        {
+      } else if (avgMarks >= 70) 
+      {
+            grade = 'C';
+      } else if (avgMarks >= 60) 
+      {
             grade = 'D';
-        }
-        else if(avgMarks<60)
-        {
-            grade  = 'F';
-        }
-        cout<<"Total marks: "<<totalMarks<<"\n";
-        cout<<"Average Marks: "<<avgMarks<<"\n";
-        cout<<"Grade : "<<grade<<"\n\n";
+      } else 
+      {
+            grade = 'F';
+      }
+
+      cout << "Total marks: " << totalMarks <<endl;
+      cout << "Average Marks: " << avgMarks <<endl;
+      cout << "Grade: " << grade <<endl;
     }
+
     return 0;
 }
