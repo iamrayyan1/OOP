eac719dcf6a45f67d96ead1fca5b614980f49b45
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-02-10 17:11:27 +0500

    Create Q1.cpp

---
+++ LABS/02/Q1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void recursiveSwap(int& a, int& b, int n)
+{
+  if(n == 0)
+  {
+    return;
+  }
+  else
+  {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  recursiveSwap(a, b, n-1);
+}
+
+}
+
+int main()
+{
+  int a, b;
+  cout << "Enter the value of a: ";
+  cin >> a;
+  cout << "Enter the value of b: ";
+  cin >> b;
+  int n;
+  cout << "Enter the number of times to swap: ";
+  cin >> n;
+  cout << "Before swapping: a = " << a << ", b = " << b << endl;
+  recursiveSwap(a, b, n);
+  cout << "After swapping: a = " << a << ", b = " << b << endl;
+  return 0;
+}
