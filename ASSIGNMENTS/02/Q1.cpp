c66bfed34b2b250c8478ac06d2d8bcbdc7e491f3
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-03-03 20:41:24 +0500

    Update Q1.cpp

--- ASSIGNMENTS/02/Q1.cpp
+++ ASSIGNMENTS/02/Q1.cpp
@@ -1,1 +1,77 @@
+#include<iostream>
+using namespace std;
 
+class Square
+{
+private:
+  float sideLength;
+  float area;          
+  static float allareas;
+
+public:
+  Square(float sideLength)
+  {
+    this->sideLength = sideLength;
+  }
+  Square() {}
+
+  void calculateArea()
+  {
+    area = sideLength * sideLength;
+    allareas += area;
+  }
+
+  float getSideLength()
+  {
+    return sideLength;
+  }
+  float getArea()
+  {
+    return area;
+  }
+  float getAllareas()
+  {
+    return allareas;
+  }
+  void setSideLength(float sideLength)
+  {
+    this->sideLength = sideLength;
+  }
+  void setArea(float area)
+  {
+    this->area = area;
+  }
+  void setAllareas(float allareas)
+  {
+    this->allareas = allareas;
+  }
+
+  display(Square square)
+  {
+    cout << "Square Details:" << endl;
+    cout << "Side Length: " << sideLength << endl;
+    cout << "Area: " << area << endl;
+    cout << "Total area: " << allareas << endl;
+  }
+
+};
+
+float Square::allareas = 0;
+
+int main()
+{
+  Square s1(3);
+  Square s2(6);
+  Square s3(11);
+  
+  s1.calculateArea();
+  s2.calculateArea();
+  s3.calculateArea();
+
+  s1.display();
+  s2.display();
+  s3.display();
+  return 0;
+  
+  
+}
