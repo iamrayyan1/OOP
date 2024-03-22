57a34dc7f0c571e035d43d093601a426da9a654c
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-03-22 05:37:43 +0500

    Update Q1.cpp

--- LABS/06/Q1.cpp
+++ LABS/06/Q1.cpp
@@ -23,4 +23,57 @@ public:
     this->numTransactions = numTransactions;
   }
 
+  BankAccount(const BankAccount& other){
+    this->accountId = other.accountId;
+    this->balance = other.balance;
+    this->numTransactions = other.numTransactions;
+    this->transactionHistory = new int[other.numTransactions];
+    for (int i = 0; i < other.numTransactions; i++)
+    {
+      this->transactionHistory[i] = other.transactionHistory[i];
+    }
+  }
+
+  ~BankAccount()
+  {
+    delete[] transactionHistory;
+  }
+
+  void display()
+  {
+    cout << "Account ID: " << accountId << endl;
+    cout << "Balance: " << balance << endl;
+    cout << "Transaction History: ";
+    for (int i = 0; i < numTransactions; i++)
+    {
+      cout << transactionHistory[i] << " ";
+    }
+    cout << endl;
+  }
+
+  void updateTransactionHistory(int* TransactionHistory, int NumTransactions)  
+  {
+    delete[] transactionHistory;
+    transactionHistory = new int[NumTransactions];
+    for (int i = 0; i < NumTransactions; i++)
+    {
+      transactionHistory[i] = TransactionHistory[i];
+    }
+    numTransactions = NumTransactions;
+  }
+
 };
+
+int main()
+{
+  int transactionHistory1[] = {1300, 2000, 300};
+  int transactionHistory2[] = {100, 200, 400};
+  BankAccount account1(1, 1000, transactionHistory1, 3);
+  BankAccount account2(2, 500, transactionHistory2, 3);
+  account1.display();
+  account2.display();
+  account1.updateTransactionHistory(transactionHistory2, 3);
+  account1.display();
+  account2.display();
+  return 0;
+}
